f7458fa14be2893b28bdad0e2c184fd2b1cee64e
Stratiz <36082806+Stratiz@users.noreply.github.com>
2022-09-25 23:23:01 -0500

    change: server.js now uses apollo to wrap express

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,12 @@
+//@ts-check
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,5 +19,57 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  //app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const {
+  ApolloServerPluginLandingPageLocalDefault
+} = require('apollo-server-core');
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  cache: 'bounded',
+  /**
+   * What's up with this embed: true option?
+   * These are our recommended settings for using AS;
+   * they aren't the defaults in AS3 for backwards-compatibility reasons but
+   * will be the defaults in AS4. For production environments, use
+   * ApolloServerPluginLandingPageProductionDefault instead.
+  **/
+  plugins: [
+    ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+  ],
+});
+
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  db.once('open', () => {
+    app.listen(PORT, () => {  
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+}
+
+startApolloServer();
+
+  
+
+// The `listen` method launches a web server.
+/*server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});*/
